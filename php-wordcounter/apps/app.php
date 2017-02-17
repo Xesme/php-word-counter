28cce7f9818d9cb4f308fc178ea59c6bfa28dadc
xa <xiaesmeamen@me.com>
2017-02-17 13:21:45 -0800

    debug date and timezone set in apps/app.php

--- php-wordcounter/apps/app.php
+++ php-wordcounter/apps/app.php
@@ -1,4 +1,5 @@
 <?php
+    date_default_timezone_set('America/Los_Angeles');
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/wordcount.php";
 
@@ -8,8 +9,13 @@
     // }
 
     $app = new Silex\Application();
-        $app->regist(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
+        $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
 
+    $app->get("/", function() use($app) {
+        return $app['twig']->render('search_form.html.twig');
+    });
+
+    return $app;
 ?>
 
 <!-- <!DOCTYPE html>
@@ -32,7 +38,7 @@
     </body>
 </html> -->
 
-<!-- 
+<!--
 <!DOCTYPE html>
 <html>
     <head>
