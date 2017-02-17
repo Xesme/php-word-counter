6bc2aed75a9ad8c7514ce9468024e1b21c8723c8
xa <xiaesmeamen@me.com>
2017-02-17 12:28:38 -0800

    add test for counting how many times a word appears in sentence(array) and debug it

--- php-wordcounter/src/wordcount.php
+++ php-wordcounter/src/wordcount.php
@@ -2,19 +2,44 @@
 
     class RepeatCounter
     {
-
+        // private $word;
+        // private $sentence;
+        // private $counter;
+        //
+        // function __construct($word, $sentence, $count)
+        // {
+        //     $this->word = $word;
+        //     $this->sentence = $sentence;
+        //     $this->counter = $counter;
+        // }
 
 
         // Functions
-        function wordEquality()
-        {
-            $sentence = explode(" ", $input_sentence);
-            $word = $input_word;
+        // function wordEquality()
+        // {
+        //     if ($input_one === $input_two)
+        //     {
+        //         echo "These words are even";
+        //         return true;
+        //     } else {
+        //         echo "These are not even";
+        //         return false;
+        //     }
+        // }
 
-            if ($word === $sentence[1])
+        function countRepeatsOne($input_word, $input_sentence)
+        {
+            $input_sentence = explode(" ", $input_sentence);
+            $counter = 0;
+
+            for ($i = 0; $i <= $input_sentence[$i]; $i++)
             {
-                return true;
+                if (in_array("$input_word", $input_sentence))
+                {
+                    $counter ++;
+                }
             }
+            return $counter;
         }
     }
 
--- php-wordcounter/tests/WordCountTest.php
+++ php-wordcounter/tests/WordCountTest.php
@@ -3,28 +3,34 @@
 
     class RepeatCounterTest extends PHPUnit_Framework_TestCase
     {
-        function test_equalWords()
+        // function test_equalWords()
+        // {
+        //     // Arrange
+        //     $test_RepeatCounter = new RepeatCounter;
+        //     $input_one = "that";
+        //     $input_two= "this";
+        //
+        //     // Act
+        //     $result = $test_RepeatCounter->wordEquality($input_one, $input_two);
+        //
+        //     // Assert
+        //     $this->assertEquals(true, $result);
+        // }
+
+
+        function test_countRepeatsOne()
         {
             // Arrange
-            $test_ReapeatCounter = new RepeatCounter;
-            $input_sentence = "the the";
-            $input_word = "the";
+            $test_RepeatCounter = new RepeatCounter;
+            $input_word = "that";
+            $input_sentence = "this or that is then or now";
+
 
             // Act
-            $result = $test_ReapeatCounter->wordEquality($input_sentence, $input_word);
+            $result = $test_RepeatCounter->countRepeatsOne($input_word, $input_sentence);
 
             // Assert
-            $this->assertEquals(true, $result);
-        }
-
-
-        function test_countRepeats()
-        {
-            // Arrange
-
-            // Act
-
-            // Assert
+            $this->assertEquals(1, $result);
 
         }
     }
