a23ce2e4e876039fb2f4e29a4241e0a60007f645
xa <xiaesmeamen@me.com>
2017-02-26 15:41:13 -0800

    fix undefined offset and remove test_wordEquality in phpunit tests

--- tests/RepeatCounterTest.php
+++ tests/RepeatCounterTest.php
@@ -3,26 +3,12 @@
 
     class RepeatCounterTest extends PHPUnit_Framework_TestCase
     {
-        function test_equalWords()
-        {
-            // Arrange
-            $test_RepeatCounter = new RepeatCounter;
-            $word = "That";
-            $sentence = "that";
-
-            // Act
-            $result = $test_RepeatCounter->wordEquality($word, $sentence);
-
-            // Assert
-            $this->assertEquals(true, $result);
-        }
-
         function test_countRepeats()
         {
             // Arrange
-            $test_RepeatCounter = new RepeatCounter;
             $word = "that";
             $sentence = "that is that";
+            $test_RepeatCounter = new RepeatCounter($word, $sentence);
 
             // Act
             $result = $test_RepeatCounter->countRepeats($word, $sentence);
