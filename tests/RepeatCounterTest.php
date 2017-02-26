45ae2d4b61101c512a5111a358a39a9174f764e7
xa <xiaesmeamen@me.com>
2017-02-26 15:29:59 -0800

    rename .php files to match class names with upper camel case

--- tests/RepeatCounterTest.php
+++ tests/RepeatCounterTest.php
@@ -1,5 +1,5 @@
 <?php
-    require_once __DIR__."/../src/wordcount.php";
+    require_once __DIR__."/../src/RepeatCounter.php";
 
     class RepeatCounterTest extends PHPUnit_Framework_TestCase
     {
